6774b286eba4c960f5672669b2dc2c44bd00a1fb
Kousuke Kikuchi <k.kikuchi@yzrh.jp>
2023-02-27 07:17:46 +0900

    added mystack

--- src/solutions.rs
+++ src/solutions.rs
@@ -3,3 +3,4 @@ mod dynamic_arrays;
 mod singly_linked_list;
 mod stacks;
 mod static_arrays;
+mod queues;
--- src/solutions/queues.rs
+++ src/solutions/queues.rs
@@ -19,12 +19,56 @@ impl Solution {
     }
 }
 
+struct MyStack {
+    original: Vec<i32>,
+    reversed: Vec<i32>,
+}
+
+impl MyStack {
+    fn new() -> Self {
+        Self {
+            original: vec![],
+            reversed: vec![],
+        }
+    }
+
+    fn push(&mut self, x: i32) {
+        self.original.insert(0, x);
+        self.reversed.insert(self.reversed.len(), x);
+    }
+
+    fn pop(&mut self) -> i32 {
+        self.reversed.remove(self.reversed.len() - 1);
+        self.original.remove(0)
+    }
+
+    fn top(&self) -> i32 {
+        self.original[0]
+    }
+
+    fn empty(&self) -> bool {
+        self.reversed.len() == 0
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::solutions::queues::Solution;
+
+    use super::MyStack;
     #[test]
     fn test_count_students() {
         let actual = Solution::count_students(vec![1, 0, 0, 1], vec![0, 1, 0, 1]);
         assert_eq!(actual, 0);
+    }
+
+    #[test]
+    fn test_my_stack() {
+        let mut stack = MyStack::new();
+        stack.push(1);
+        stack.push(2);
+        assert_eq!(stack.top(), 2);
+        assert_eq!(stack.pop(), 2);
+        assert_eq!(stack.empty(), false);
     }
 }
