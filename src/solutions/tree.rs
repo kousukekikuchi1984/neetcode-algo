60b422cd7200c6870a6cc014351c2cbbfa72a75e
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-03-27 06:59:47 +0900

    added k_closest

--- src/solutions/tree.rs
+++ src/solutions/tree.rs
@@ -86,7 +86,22 @@ impl Solution {
         }
     }
 
-    pub fn k_closest(points: Vec<Vec<i32>>, k: i32) -> Vec<Vec<i32>> {}
+    pub fn k_closest(points: Vec<Vec<i32>>, k: i32) -> Vec<Vec<i32>> {
+        let mut heap = BinaryHeap::new();
+        for point in points {
+            // no need to calculate square root of distance
+            let distance = point[0] * point[0] + point[1] * point[1];
+            heap.push((distance, point));
+            if heap.len() > k as usize {
+                heap.pop();
+            }
+        }
+
+        heap.into_vec()
+            .into_iter()
+            .map(|(_, point)| point)
+            .collect()
+    }
 }
 
 struct KthLargest {
