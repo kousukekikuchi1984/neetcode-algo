d427f8d01318ea0cccf2b1346efe1e66e4173408
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-03-31 07:08:31 +0900

    added num_islands but need some improvements

--- src/solutions/graph.rs
+++ src/solutions/graph.rs
@@ -1,8 +1,50 @@
+use std::collections::HashSet;
+
 struct Solution {}
 
 impl Solution {
     pub fn num_islands(grid: Vec<Vec<char>>) -> i32 {
-        1
+        fn _num_islands(
+            grid: &Vec<Vec<char>>,
+            current: (isize, isize),
+            arrived: &mut HashSet<(isize, isize)>,
+        ) {
+            let (x, y) = current;
+            // end recursive when: out of bounds, already arrived,
+            if x < 0
+                || y < 0
+                || x >= grid.len() as isize
+                || y >= grid[0].len() as isize
+                || arrived.contains(&current)
+            {
+                return;
+            }
+
+            // end recursive when: current is water
+            if grid[x as usize][y as usize] == '0' {
+                return;
+            }
+
+            // update arrived
+            arrived.insert(current);
+
+            _num_islands(grid, (x + 1, y), arrived);
+            _num_islands(grid, (x - 1, y), arrived);
+            _num_islands(grid, (x, y + 1), arrived);
+            _num_islands(grid, (x, y - 1), arrived);
+        }
+
+        let mut arrived: HashSet<(isize, isize)> = HashSet::new();
+        let mut islands = 0;
+        for x in 0..grid.len() {
+            for y in 0..grid[0].len() {
+                if grid[x][y] == '1' && !arrived.contains(&(x as isize, y as isize)) {
+                    _num_islands(&grid, (x as isize, y as isize), &mut arrived);
+                    islands += 1;
+                }
+            }
+        }
+        islands
     }
 }
 
@@ -12,14 +54,14 @@ mod tests {
 
     #[test]
     fn test_num_islands() {
-        let grid = vec![
-            vec!["1", "1", "1", "1", "0"],
-            vec!["1", "1", "0", "1", "0"],
-            vec!["1", "1", "0", "0", "0"],
-            vec!["0", "0", "0", "0", "0"],
+        let grid: Vec<Vec<char>> = vec![
+            vec!['1', '1', '0', '0', '0'],
+            vec!['1', '1', '0', '0', '0'],
+            vec!['0', '0', '1', '0', '0'],
+            vec!['0', '0', '0', '1', '1'],
         ];
         let actual = Solution::num_islands(grid);
-        let expected = 1;
+        let expected = 3;
         assert_eq!(actual, expected);
     }
 }
