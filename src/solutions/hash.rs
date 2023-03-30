648af3896e482f7638ff584b4912c0cb2285fbfa
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-03-30 21:33:19 +0900

    added Linkedlist HashMap

--- src/solutions/hash.rs
+++ src/solutions/hash.rs
@@ -1,5 +1,5 @@
-use std::borrow::Borrow;
 use std::collections::HashSet;
+use std::collections::LinkedList;
 use std::collections::{HashMap, VecDeque};
 
 struct Solution {}
@@ -111,6 +111,67 @@ impl MyHashSet {
     }
 }
 
+const BUCKET_COUNT: usize = 100;
+
+#[derive(Default)]
+struct MyHashMap {
+    buckets: Vec<LinkedList<(i32, i32)>>,
+}
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl MyHashMap {
+    /** Initialize your data structure here. */
+    fn new() -> Self {
+        Self {
+            buckets: vec![LinkedList::new(); BUCKET_COUNT],
+        }
+    }
+
+    fn key_hash(key: i32) -> usize {
+        (key as usize) % BUCKET_COUNT
+    }
+
+    /** value will always be non-negative. */
+    fn put(&mut self, key: i32, value: i32) {
+        let hash = Self::key_hash(key);
+        for node in self.buckets[hash].iter_mut() {
+            if node.0 == key {
+                node.1 = value;
+                return;
+            }
+        }
+        self.buckets[hash].push_back((key, value));
+    }
+
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    fn get(&self, key: i32) -> i32 {
+        let hash = Self::key_hash(key);
+        for node in self.buckets[hash].iter() {
+            if node.0 == key {
+                return node.1;
+            }
+        }
+        -1
+    }
+
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    fn remove(&mut self, key: i32) {
+        let hash = Self::key_hash(key);
+        let idx = self.buckets[hash]
+            .iter()
+            .enumerate()
+            .find_map(|(idx, node)| if node.0 == key { Some(idx) } else { None });
+        if let Some(idx) = idx {
+            let mut split_list = self.buckets[hash].split_off(idx);
+            split_list.pop_front();
+            self.buckets[hash].append(&mut split_list);
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -146,5 +207,18 @@ mod tests {
         assert_eq!(hash_set.contains(2), true);
         hash_set.remove(2);
         assert_eq!(hash_set.contains(2), false);
+    }
+
+    #[test]
+    fn test_my_hash_map() {
+        let mut hash_map = MyHashMap::new();
+        hash_map.put(1, 1);
+        hash_map.put(2, 2);
+        assert_eq!(hash_map.get(1), 1);
+        assert_eq!(hash_map.get(3), -1);
+        hash_map.put(2, 1);
+        assert_eq!(hash_map.get(2), 1);
+        hash_map.remove(2);
+        assert_eq!(hash_map.get(2), -1);
     }
 }
