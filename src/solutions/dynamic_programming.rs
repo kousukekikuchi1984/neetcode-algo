927b295c81f9fbf2bdb82eb180f9b0f010c0ed9f
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-04-04 08:43:29 +0900

    added rob

--- src/solutions/dynamic_programming.rs
+++ src/solutions/dynamic_programming.rs
@@ -1,8 +1,16 @@
+use std::cmp::max;
+
 struct Solution {}
 
 impl Solution {
     pub fn rob(nums: Vec<i32>) -> i32 {
-
+        let mut dp = vec![0; nums.len() + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for i in 1..nums.len() {
+            dp[i + 1] = max(dp[i], dp[i - 1] + nums[i]);
+        }
+        dp[nums.len()]
     }
 }
 
@@ -12,8 +20,7 @@ mod tests {
 
     #[test]
     fn test_rob() {
-        // leetcode: https://leetcode.com/problems/house-robber/
-        // example 1
+        // ref: https://leetcode.com/problems/house-robber/
         let nums = vec![1, 2, 3, 1];
         let expected = 4;
         let actual = Solution::rob(nums);
