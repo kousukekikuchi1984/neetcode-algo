778fbe7af9c6169b56941d018adea19fa6b2f70e
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-04-06 06:13:19 +0900

    added longest_common_subsequence but need to modify

--- src/solutions/dynamic_programming.rs
+++ src/solutions/dynamic_programming.rs
@@ -1,4 +1,5 @@
 use std::cmp::max;
+use std::cmp::Ordering;
 
 struct Solution {}
 
@@ -40,6 +41,21 @@ impl Solution {
         }
         dp[obstacle_grid.len() - 1][obstacle_grid[0].len() - 1]
     }
+
+    pub fn longest_common_subsequence(text1: String, text2: String) -> i32 {
+        // using Dynamic Programming
+        let mut dp = vec![vec![0; text2.len() + 1]; text1.len() + 1];
+        for i in 0..text1.len() {
+            for j in 0..text2.len() {
+                match text1.chars().nth(i).unwrap().cmp(&text2.chars().nth(j).unwrap()) {
+                    Ordering::Equal => dp[i + 1][j + 1] = dp[i][j] + 1,
+                    _ => dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j]),
+                }
+            }
+        }
+
+        dp[text1.len()][text2.len()]
+    }
 }
 
 #[cfg(test)]
@@ -71,6 +87,16 @@ mod tests {
         let obstacle_grid = vec![vec![0, 0, 0], vec![0, 1, 0], vec![0, 0, 0]];
         let expected = 2;
         let actual = Solution::unique_paths_with_obstacles(obstacle_grid);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_longest_common_subsequence() {
+        // ref: https://leetcode.com/problems/longest-common-subsequence/
+        let text1 = "abcde".to_string();
+        let text2 = "ace".to_string();
+        let expected = 3;
+        let actual = Solution::longest_common_subsequence(text1, text2);
         assert_eq!(expected, actual);
     }
 }
