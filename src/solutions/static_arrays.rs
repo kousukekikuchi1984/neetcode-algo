ba0b358a1c060e75d65d19d10be03f15cc545d0d
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-04-07 07:20:13 +0900

    added max_subarray_sum_circular

--- src/solutions/static_arrays.rs
+++ src/solutions/static_arrays.rs
@@ -1,4 +1,4 @@
-use std::cmp::max;
+use std::cmp::{max, min};
 
 struct Solution {}
 
@@ -33,7 +33,25 @@ impl Solution {
         max_sum
     }
 
-    pub fn max_subarray_sum_circular(nums: Vec<i32>) -> i32 {}
+    pub fn max_subarray_sum_circular(nums: Vec<i32>) -> i32 {
+        let (mut global_max, mut global_min) = (nums[0], nums[0]);
+        let (mut current_max, mut current_min) = (0, 0);
+        let mut total = 0;
+
+        for num in nums {
+            current_max = i32::max(num, current_max + num);
+            current_min = i32::min(num, current_min + num);
+            total += num;
+            global_max = i32::max(global_max, current_max);
+            global_min = i32::min(global_min, current_min);
+        }
+
+        return if global_max > 0 {
+            i32::max(global_max, total - global_min)
+        } else {
+            global_max
+        }
+    }
 }
 
 #[cfg(test)]
