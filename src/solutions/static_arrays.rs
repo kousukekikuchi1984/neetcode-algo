7c02723cd1210639605045f6d210980dcff8c995
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-04-15 05:51:08 +0900

    added skelton for two_sum

--- src/solutions/static_arrays.rs
+++ src/solutions/static_arrays.rs
@@ -201,6 +201,10 @@ impl Solution {
         }
         true
     }
+
+    pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
+
+    }
 }
 
 #[cfg(test)]
@@ -305,5 +309,11 @@ mod tests {
         assert_eq!(Solution::is_palindrome("race a car".to_string()), false);
         assert_eq!(Solution::is_palindrome(" ".to_string()), true);
         assert_eq!(Solution::is_palindrome("a.".to_string()), true);
+    }
+
+    #[test]
+    fn test_two_sum() {
+        // https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+        assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![1, 2]);
     }
 }
