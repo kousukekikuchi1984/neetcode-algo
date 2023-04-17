eaf94924556a26fd655a07f544beaff775c28888
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-04-18 05:56:16 +0900

    added numarray

--- src/solutions/static_arrays.rs
+++ src/solutions/static_arrays.rs
@@ -290,20 +290,27 @@ impl Solution {
 }
 
 struct NumArray {
-
+    sum: Vec<i32>,
 }
 
 impl NumArray {
+    fn new(nums: Vec<i32>) -> Self {
+        let mut sum = vec![0; nums.len() + 1];
+        for i in 0..nums.len() {
+            sum[i + 1] = sum[i] + nums[i];
+        }
+        Self { sum }
+    }
 
-    fn new(nums: Vec<i32>) -> Self {}
-
-    fn sum_range(&self, left: i32, right: i32) -> i32 {}
+    fn sum_range(&self, left: i32, right: i32) -> i32 {
+        self.sum[right as usize + 1] - self.sum[left as usize]
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::Solution;
     use super::NumArray;
+    use super::Solution;
 
     #[test]
     fn test_remove_element() {
