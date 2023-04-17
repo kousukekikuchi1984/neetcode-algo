7fa64c878e495ff076a938a2be6cb62cd1c9fa5e
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-04-18 06:07:47 +0900

    add sum region

--- src/solutions/static_arrays.rs
+++ src/solutions/static_arrays.rs
@@ -307,9 +307,18 @@ impl NumArray {
     }
 }
 
+struct NumMatrix {}
+
+impl NumMatrix {
+    fn new(matrix: Vec<Vec<i32>>) -> Self {}
+
+    fn sum_region(&self, row1: i32, col1: i32, row2: i32, col2: i32) -> i32 {}
+}
+
 #[cfg(test)]
 mod tests {
     use super::NumArray;
+    use super::NumMatrix;
     use super::Solution;
 
     #[test]
@@ -447,5 +456,20 @@ mod tests {
         assert_eq!(obj.sum_range(0, 2), 1);
         assert_eq!(obj.sum_range(2, 5), -1);
         assert_eq!(obj.sum_range(0, 5), -3);
+    }
+
+    #[test]
+    fn test_num_matrix_query() {
+        let nums = vec![
+            vec![3, 0, 1, 4, 2],
+            vec![5, 6, 3, 2, 1],
+            vec![1, 2, 0, 1, 5],
+            vec![4, 1, 0, 1, 7],
+            vec![1, 0, 3, 0, 5],
+        ];
+        let obj =NumMatrix::new(nums);
+        assert_eq!(obj.sum_region(2, 1, 4, 3), 8);
+        assert_eq!(obj.sum_region(1, 1, 2, 2), 11);
+        assert_eq!(obj.sum_region(1, 2, 2, 4), 12);
     }
 }
