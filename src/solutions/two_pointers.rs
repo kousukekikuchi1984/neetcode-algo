e4c7195a6192f42e4e8330adb9b630424e5e5b4b
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-05-31 06:27:19 +0900

    added three_sum

--- src/solutions/two_pointers.rs
+++ src/solutions/two_pointers.rs
@@ -1,19 +1,45 @@
+use std::collections::{HashSet, HashMap};
+
 struct Solution {}
 
 impl Solution {
     pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        vec![vec![-1, -1, 2], vec![-1, 0, 1]]
+        // 3 <= nums.length <= 3000
+        // n^2 and hashmap
+        let mut three = HashMap::with_capacity(nums.len());
+        for i in 0..nums.len() {
+            three.insert(nums[i], i);
+        }
+        let mut result: HashSet<Vec<i32>> = HashSet::new();
+        for i in 0..nums.len() {
+            for j in i + 1..nums.len() {
+                let expected_value = (nums[i] + nums[j]) * -1;
+                if three.contains_key(&expected_value) {
+                    let index = three.get(&expected_value).unwrap();
+                    if *index > j {
+                        let mut triplet = vec![nums[i], nums[j], expected_value];
+                        triplet.sort();
+                        result.insert(triplet);
+                    }
+                }
+            }
+        }
+        result.into_iter().collect()
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashSet;
     use super::Solution;
 
     #[test]
     fn test_three_sum() {
         let nums = vec![-1, 0, 1, 2, -1, -4];
-        let expected = vec![vec![-1, -1, 2], vec![-1, 0, 1]];
-        assert_eq!(Solution::three_sum(nums), expected);
+        let expected_vec = vec![vec![-1, -1, 2], vec![-1, 0, 1]];
+        let expected: HashSet<Vec<i32>> = HashSet::from_iter(expected_vec);
+        let act_vec = Solution::three_sum(nums);
+        let actual: HashSet<Vec<i32>> = HashSet::from_iter(act_vec);
+        assert_eq!(actual, expected);
     }
 }
