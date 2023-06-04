66ec187f76f093bae55f7954b11aa56a8dcb6e01
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-06-05 06:46:10 +0900

    added search

--- src/solutions/tree.rs
+++ src/solutions/tree.rs
@@ -127,7 +127,28 @@ impl Solution {
     }
 
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-        4
+        let mut left = 0;
+        let mut right = nums.len() - 1;
+        while left <= right {
+            let mid = (left + right) / 2;
+            if target == nums[mid] {
+                return mid as i32;
+            }
+            if nums[left] <= nums[mid] {
+                if target < nums[left] || target > nums[mid] {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                if target < nums[mid] || target > nums[right] {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        -1
     }
 }
 
