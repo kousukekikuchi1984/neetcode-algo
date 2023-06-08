d48c0c42c800abab605c4a199382039fd1de0a7e
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-06-09 06:27:53 +0900

    added plus_one

--- src/solutions/math.rs
+++ src/solutions/math.rs
@@ -2,7 +2,23 @@ struct Solution {}
 
 impl Solution {
     pub fn plus_one(digits: Vec<i32>) -> Vec<i32> {
-        vec![4, 3, 2, 2]
+        let mut carry = 1;
+        let mut digits = digits.clone();
+
+        digits = digits
+            .into_iter()
+            .rev()
+            .map(|digit| {
+                let val = digit + carry;
+                carry = val / 10;
+                (val % 10)
+            })
+            .collect();
+        digits.reverse();
+        if carry == 1 {
+            digits.insert(0, 1);
+        }
+        digits
     }
 }
 
@@ -10,8 +26,9 @@ impl Solution {
 mod tests {
     use super::*;
 
+    #[test]
     fn test_plus_one() {
         assert_eq!(Solution::plus_one(vec![4, 3, 2, 1]), vec![4, 3, 2, 2]);
-        assert_eq!(Solution::plus_one(vec![0]), vec![1, 0]);
+        assert_eq!(Solution::plus_one(vec![9]), vec![1, 0]);
     }
 }
